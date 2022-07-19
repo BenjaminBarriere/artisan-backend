8b19e8687bf9b5ec1a7b5e00542b471f1f7ff996
Benjamin BARRIERE <wyyzer@gmail.com>
2022-07-19 11:51:05 +0200

    Json size limit increased

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ require("dotenv").config()
 
 app.use(cors())
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
+app.use(express.json({ limit: "50mb" }))
 
 app.use("/images", express.static("images"))
 
